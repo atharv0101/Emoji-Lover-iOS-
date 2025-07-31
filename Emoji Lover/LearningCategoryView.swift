27ab1c3de4b277c7098c04dacb069ac41321ccc4
atharv <atharv1002@gmail.com>
2025-07-31 12:39:54 +0530

    app implementation -> step 1

---
+++ Emoji Lover/LearningCategoryView.swift
@@ -0,0 +1,42 @@
+//
+//  Untitled.swift
+//  Emoji Lover
+//
+//  Created by Atharv Maheshwari on 31/07/25.
+//
+
+import AVFoundation
+
+struct LearningCategoryView: View {
+    let title: String
+    let items: [LearningItem]
+    @State private var currentIndex = 0
+
+    var body: some View {
+        VStack(spacing: 30) {
+            Text(items[currentIndex].emoji)
+                .font(.system(size: 150))
+
+            Text(items[currentIndex].name)
+                .font(.largeTitle)
+                .bold()
+
+            Button("🔊 Speak") {
+                let utterance = AVSpeechUtterance(string: items[currentIndex].name)
+                utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+                AVSpeechSynthesizer().speak(utterance)
+            }
+
+            HStack {
+                Button("◀️") {
+                    if currentIndex > 0 { currentIndex -= 1 }
+                }
+
+                Button("▶️") {
+                    if currentIndex < items.count - 1 { currentIndex += 1 }
+                }
+            }
+        }
+        .padding()
+    }
+}
---
+++ Emoji Lover/LearningData.swift
@@ -0,0 +1,7 @@
+//
+//  Learning.swift
+//  Emoji Lover
+//
+//  Created by Atharv Maheshwari on 31/07/25.
+//
+
